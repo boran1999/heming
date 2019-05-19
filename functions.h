96a7d8cc3e86d5cd10ccc42d518515506a593121
Anton Borisov <32795987+boran1999@users.noreply.github.com>
2019-05-19 20:51:25 +0300

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,29 @@
+#pragma once
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int get_amount_simbols(const char* filename) {
+	ifstream in(filename);
+	int N = 0;
+	char sym;
+	while (!in.eof()) {
+		in >> sym;
+		N++;
+	}
+	N -= 1;
+	return N;
+}
+
+void setBit(char& b, int i, char bit)
+{
+	b = (bit == 1) ? b | (1 << i) : b & ~(1 << i);
+}
+
+char getBit(char b, int i)
+{
+	b = b & (1 << i);
+	b = b != 0 ? 1 : 0;
+	return b;
+}
